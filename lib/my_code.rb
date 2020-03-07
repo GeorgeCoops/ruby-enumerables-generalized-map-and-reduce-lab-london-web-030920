7834d987f555a6882dad2fd0bef55f6bab5bb7cb
George Cooper <coopsgeorge@gmail.com>
2020-03-07 12:02:22 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,18 +9,17 @@ end
 new_array
 end
 
-def reduce(array, starting_value=nil)
-  if starting_value
-    sum = starting_value
+def reduce(array, sv=nil)
+  if sv
+    sum = sv
     i = 0
   else
     sum = array[0]
-    i = 0
+    i = 1
   end
   while i < array.length
     sum = yield(sum, array[i])
     i += 1
   end
   sum 
-
 end
